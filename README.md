e299d66e0441b6d6a6dea8eb9f558506ac8c1f61
Joshua Minor <joshm@pixar.com>
2022-11-10 20:46:16 -0800

    Simple Swift example

---
+++ README.md
@@ -0,0 +1,33 @@
+# OpenTimelineIO C++ Example
+
+This repository serves as a minimal example of using [OpenTimelineIO](https://opentimeline.io) in a Swift application.
+
+Allowable use of the code in this repository includes copying and pasting portions into
+your own projects without attribution.
+
+
+# Getting started
+
+To get started, use git to clone this repository:
+
+```
+git clone https://github.com/OpenTimelineIO/otio-swift-example.git
+```
+
+...then open the `otio-swift-example.xcodeproj` in Xcode, and press Run.
+
+
+Please also consult with the OpenTimelineIO [documentation](https://opentimelineio.readthedocs.io/en/latest/index.html)
+for more information about OpenTimelineIO in general.
+
+Good luck and happy coding!
+
+
+## Contributions
+
+If you have any suggested changes to the template repository itself, 
+please provide them via [pull request](../../pulls) or [create an issue](../../issues) as appropriate. 
+
+All contributions back to the template repository must align with the contribution
+[guidelines](https://opentimelineio.readthedocs.io/en/latest/tutorials/contributing.html) 
+of the OpenTimelineIO project.
--- otio-swift-example/main.swift
+++ otio-swift-example/main.swift
@@ -2,10 +2,75 @@
 //  main.swift
 //  otio-swift-example
 //
-//  Created by Joshua Minor on 11/10/22.
-//
 
 import Foundation
+import OpenTimelineIO
 
-print("Hello, World!")
+if CommandLine.arguments.count != 2 {
+    print("Usage: \(CommandLine.arguments[0]) <file.otio>")
+    exit(1)
+}
 
+let path = CommandLine.arguments[1]
+
+let object: SerializableObject
+do {
+    object = try OpenTimelineIO.SerializableObject.fromJSON(filename: path)
+} catch {
+    print("Error: \(error)")
+    exit(2)
+}
+
+print("Loaded OTIO file: \(path)")
+
+guard let timeline = object as? OpenTimelineIO.Timeline
+else {
+    print("Error: \(object) is not a Timeline.")
+    exit(3)
+}
+
+do {
+    print("Timeline name: \(timeline.name)")
+    print("Timeline duration: \(try timeline.duration().toTimecode())")
+
+    print("Video Tracks:")
+    if timeline.videoTracks.count == 0 {
+        print("  No video tracks")
+    }else{
+        for track in timeline.videoTracks {
+            print("  Track: \(track.name)")
+            print("    Kind: \(track.kind)")
+            print("    Duration: \(try track.duration().toTimecode())")
+
+            for clip in track.children {
+                print("  Clip: \(clip.name)")
+                print("    Duration: \(try clip.duration().toTimecode())")
+            }
+        }
+    }
+
+    print("Audio Tracks:")
+    if timeline.audioTracks.count == 0 {
+        print("  No audio tracks")
+    }else{
+        for track in timeline.audioTracks {
+            print("  Track: \(track.name)")
+            print("    Kind: \(track.kind)")
+            print("    Duration: \(try track.duration().toTimecode())")
+
+            for clip in track.children {
+                print("  Clip: \(clip.name)")
+                print("    Duration: \(try clip.duration().toTimecode())")
+            }
+        }
+    }
+
+    // TODO: Recursive each_clip() or clip_if() is missing?
+//    for clip in timeline.clip_if() {
+//        print("  Clip: \(clip.name)")
+//        print("    Duration: \(try clip.duration().toTimecode())")
+//    }
+
+} catch {
+    print(error)
+}
